7e37346f9b3af788f97dbf69563368cb0a299c5d
Barak Binaymin <barak.binyamin555@gmail.com>
2025-06-14 22:28:23 -0400

    oops

--- ws.js
+++ ws.js
@@ -44,7 +44,7 @@ uWS.App().ws('/*', {
   },
 
   message: (ws, message) => {
-    const event = Buffer.from(message).toString() // { id, cursorX, cursorY}
+    const event = Buffer.from(message).toString() // { cursorX, cursorY}
     event['id'] = ws.id
     queue.enqueue(JSON.parse(event))
   },
@@ -79,8 +79,8 @@ async function main(){
     const velocity   = 1
     const positionX  = state[id].x
     const positionY  = state[id].y
-    state[id].positionX = positionX>cursorX  ? positionX+velocity : positionX-velocity
-    state[id].positionY = positionY>cursorY  ? positionY+velocity : positionY-velocity
+    state[id].x = positionX>cursorX  ? positionX+velocity : positionX-velocity
+    state[id].y = positionY>cursorY  ? positionY+velocity : positionY-velocity
   }
 }
 
